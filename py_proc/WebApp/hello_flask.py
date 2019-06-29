426d33f7e926dceb1a532ed147b44c8e98acd715
Joe <qiaokun_3ok@126.com>
2019-06-29 22:02:36 +0800

    增加了log来记录请求和对应的结果

--- py_proc/WebApp/hello_flask.py
+++ py_proc/WebApp/hello_flask.py
@@ -16,6 +16,10 @@ def hello()->str:
 def entry_page()->'html':
 	return render_template('entry.html',the_title ='welcome to search4letters on the web')
 
+def log_request(req:'flask_request',res:str)->none:
+	with open('vsearch.log','a') as log:
+		print(req,res,file=log)
+
 @app.route('/search4',methods=['POST'])
 def do_search()->str:
 	phrase = request.form['phrase']
@@ -25,5 +29,12 @@ def do_search()->str:
 	return render_template('results.html',the_title=title,
 		the_phrase=phrase,the_letters=letters,the_results=result)
 
+@app.route('/viewlog')
+def view_the_log()->str:
+	with open(vsearch.log) as log:
+		contents = log.read()
+	return contents
 
-app.run() 
+if __name__ == '__main__':
+	app.run(debug = True) 
+

85c9b56117f5bec58215d02467ebb62f130c55c2
Joe <qiaokun_3ok@126.com>
2019-06-30 22:58:14 +0800

    将log日志web化但仍然存在不能可读显示的问题

--- py_proc/WebApp/hello_flask.py
+++ py_proc/WebApp/hello_flask.py
@@ -1,6 +1,6 @@
 #使用flash包输出网址
 
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,escape
 
 
 def search4letters(phrase:str, letters:str ='aeiou')-> set:
@@ -16,24 +16,34 @@ def hello()->str:
 def entry_page()->'html':
 	return render_template('entry.html',the_title ='welcome to search4letters on the web')
 
-def log_request(req:'flask_request',res:str)->none:
+def log_request(req:'flask_request',res:str)->None:
 	with open('vsearch.log','a') as log:
-		print(req,res,file=log)
+		print(req.form,res,file=log,end='|')
+		print(req.remote_addr,file=log,end='|')
+		print(req.user_agent,file=log,end='|')
+		print(res,file=log)
 
 @app.route('/search4',methods=['POST'])
 def do_search()->str:
 	phrase = request.form['phrase']
 	letters= request.form['letters']
 	title  = 'here are your result'
-	result = str(search4letters(phrase,letters))
+	results= str(search4letters(phrase,letters))
+	log_request(request,results)
 	return render_template('results.html',the_title=title,
-		the_phrase=phrase,the_letters=letters,the_results=result)
+		the_phrase=phrase,the_letters=letters,the_results=results)
 
 @app.route('/viewlog')
-def view_the_log()->str:
-	with open(vsearch.log) as log:
-		contents = log.read()
-	return contents
+def view_the_log()->'html':
+	contents = []
+	with open('vsearch.log') as log:
+		for line in log:
+			contents.append([])
+			for item in line.split('|'):
+				contents[-1].append(escape(item))
+	titles = {'form date','remote_addr','user_agent','results'}
+	return render_template('viewlog.html',the_title = 'view log',the_row_titles = 'titles',the_data = 'contents',)
+
 
 if __name__ == '__main__':
 	app.run(debug = True) 

4468ae32b60a8c5b9a552e7f7f7475c6363999c2
Joe <qiaokun_3ok@126.com>
2019-11-20 22:16:28 +0800

    增加了go语言的学习记录

---
+++ Go_proc/src/go_code/goproject01/main/hello.go
@@ -0,0 +1,8 @@
+// 输出hello world 
+package main   // 表示该文件属于main包 go语言的每个文件必须归属于一个包
+import "fmt"
+func main(){
+  fmt.Println("hello,world!\n")
+}
+
+
---
+++ Go_proc/src/go_code/goproject02/main/escapechar.go
@@ -0,0 +1,6 @@
+package main
+import "fmt"
+func main(){
+  fmt.Println("姓名\t年龄\t籍贯\t住址\t")
+  fmt.Println("冯妍\t18\t河北\t住址\t")
+}
